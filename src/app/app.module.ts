7a562d1148f085f885dd3455e4132404463b3b8c
Guilherme Melo <gui.melobarros10@hotmail.com>
2021-11-10 18:07:46 -0300

    template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from'@angular/material/sidenav';
+import {MatListModule} from'@angular/material/list';
+import {MatCardModule} from'@angular/material/card';
+import {MatButtonModule} from'@angular/material/button';
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatFormFieldModule}from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
